210a42efbb1bebb2a569f2da46665507293f909b
Maximilian Schiller <manimax3@outlook.de>
2019-12-02 21:53:42 +0100

    read filename from cmd args

--- satsolver.cpp
+++ satsolver.cpp
@@ -1,10 +1,18 @@
 #include "parser.hpp"
 #include <cstdio>
 
-int result;
+extern FILE* yyin;
 
 int main(int argc, char **argv)
 {
+    if (argc == 2) {
+        yyin = fopen(argv[1], "r");
+    }
+
     yyparse();
+
+    if (argc == 2) {
+        fclose(yyin);
+    }
     return 0;
 }
