4e06becb1e8f36b76f4a13e76b597300caffac45
Maximilian Schiller <manimax3@outlook.de>
2019-12-02 18:10:30 +0100

    Expression ast and parsing

--- ast.h
+++ ast.h
@@ -1,15 +1,117 @@
 #pragma once
 #include <cstdio>
+#include <string>
 
 class Expression {
    public:
-    Expression(Expression *lhs, Expression *rhs) : lhs(lhs), rhs(rhs) {}
+    virtual ~Expression() = default;
+    virtual void print(){};
+};
 
-    ~Expression() {
+class PredExpression : public Expression {
+   public:
+    PredExpression(const std::string &name) : name(name) {}
+	void print() override {
+		printf("%s", name.c_str());
+	}
+
+   private:
+    std::string name;
+};
+
+class ImplExpression : public Expression {
+   public:
+    ImplExpression(Expression *lhs, Expression *rhs) : lhs(lhs), rhs(rhs) {}
+
+    ~ImplExpression() {
 	if (lhs) delete lhs;
 	if (rhs) delete rhs;
     }
+	void print() override {
+		printf("(");
+		lhs->print();
+		printf(" -> ");
+		rhs->print();
+		printf(")");
+	}
 
    private:
-    Expression *lhs, *rhs;
+    Expression *lhs = nullptr, *rhs = nullptr;
+};
+
+class BiImplExpression : public Expression {
+   public:
+    BiImplExpression(Expression *lhs, Expression *rhs) : lhs(lhs), rhs(rhs) {}
+
+    ~BiImplExpression() {
+	if (lhs) delete lhs;
+	if (rhs) delete rhs;
+    }
+	void print() override {
+		printf("(");
+		lhs->print();
+		printf(" <-> ");
+		rhs->print();
+		printf(")");
+	}
+
+   private:
+    Expression *lhs = nullptr, *rhs = nullptr;
+};
+
+class AndExpression : public Expression {
+   public:
+    AndExpression(Expression *lhs, Expression *rhs) : lhs(lhs), rhs(rhs) {}
+
+    ~AndExpression() {
+	if (lhs) delete lhs;
+	if (rhs) delete rhs;
+    }
+	void print() override {
+		printf("(");
+		lhs->print();
+		printf(" and ");
+		rhs->print();
+		printf(")");
+	}
+
+   private:
+    Expression *lhs = nullptr, *rhs = nullptr;
+};
+
+class OrExpression : public Expression {
+   public:
+    OrExpression(Expression *lhs, Expression *rhs) : lhs(lhs), rhs(rhs) {}
+
+    ~OrExpression() {
+	if (lhs) delete lhs;
+	if (rhs) delete rhs;
+    }
+	void print() override {
+		printf("(");
+		lhs->print();
+		printf(" or ");
+		rhs->print();
+		printf(")");
+	}
+
+   private:
+    Expression *lhs = nullptr, *rhs = nullptr;
+};
+
+class NegExpression : public Expression {
+   public:
+    NegExpression(Expression *other) : other(other) {}
+
+    ~NegExpression() {
+	if (other) delete other;
+    }
+	void print() override {
+		printf("(¬");
+		other->print();
+		printf(")");
+	}
+
+   private:
+    Expression *other = nullptr;
 };
--- parser.y
+++ parser.y
@@ -22,21 +22,30 @@ void yyerror(char *s);
 %token OR
 %token NEGATION
 
+%right IMPLICATION BIIMPLICATION
+%left OR
+%left AND
+%precedence NEGATION
+
 %type <expression> expression
 
 %destructor {
 	printf("Destruction expression or predicate\n");
+	$$->print();
+	printf("\n");
 	delete ($$);
-} <expression> <pred>
+} <expression>
 
 %start expression
 
 %%
-expression : PREDICATE AND PREDICATE {$$ = new Expression(nullptr, nullptr); 
-			   printf("Found predicates %s and %s\n", $1->c_str(), $3->c_str());
-			   delete $1;
-			   delete $3;
-		   }
+expression : PREDICATE {$$ = new PredExpression(*$1); delete $1;}
+		   | expression IMPLICATION expression {$$ = new ImplExpression($1, $3);}
+		   | expression BIIMPLICATION expression {$$ = new BiImplExpression($1, $3);}
+		   | expression AND expression {$$ = new AndExpression($1, $3);}
+		   | expression OR expression {$$ = new OrExpression($1, $3);}
+		   | NEGATION expression {$$ = new NegExpression($2);}
+		   | '(' expression ')' { $$ = $2; }
 %%
 
 void yyerror(char *s) {
