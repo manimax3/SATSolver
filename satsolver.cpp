6531fa8bba8caeba9a145035c645d86ed225557e
Maximilian Schiller <manimax3@outlook.de>
2019-12-02 15:38:18 +0100

    Get automatic generation working

--- satsolver.cpp
+++ satsolver.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
+#include "parser.hpp"
 
 int main(int argc, char** argv)
 {
 	printf("Hello world!\n");
 	printf("%i Argument(s) have been submitted!\n", argc - 1);
+	yyparse();
 	return 0;
 }
